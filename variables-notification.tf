4032a7b5f35e352ad33d89ef44e3f06b3dd3c81d
angeline-hilda <angelinehilda97@gmail.com>
2025-03-26 12:51:24 +0530

    Updated files

---
+++ variables-notification.tf
@@ -0,0 +1,15 @@
+variable "notification_topic_description" {
+    description = "The description of the topic being created. Avoid entering confidential information."
+    type = string
+}
+
+variable "notification_topic_name" {
+  
+}
+
+/*
+variable "topic_id" {
+    description = "The OCID of the topic for the subscription."
+  
+}
+*/
---
+++ variables-subscription.tf
@@ -0,0 +1,14 @@
+
+
+variable "endpoint" {
+description = "A locator that corresponds to the subscription protocol - EMAIL, URL"
+type = string
+  
+}
+
+variable "protocol" {
+    description = "The protocol used for the subscription. - CUSTOM HTTTPS, EMAIL, HTTPS, PAGERDUTY, ORACLE_FUNCTIONS, SLACK, SMS"
+    type = string
+  
+}
+
